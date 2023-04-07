d9472c26fb336354e5382f0f70ff4632e288cd18
Michelle Okagbue <umbralcompendium@gmail.com>
2023-04-07 18:09:53 -0400

    changed the deck from array of numbers to card obj

--- Pages/Game/game.js
+++ Pages/Game/game.js
@@ -1,8 +1,6 @@
 const cards = require("../../Assets/cards.js")
 
-let suit = Array.from(Array(13)).map((element, index) => index + 1);
-let deck = suit.concat(suit, suit, suit);
-console.log("Deck of cards: ", deck);
+let deck = cards;
 let shuffle = deck.sort(() => Math.random() - 0.5);
 console.log("Shuffled deck... ", shuffle);
 
@@ -11,14 +9,14 @@ let handTwo = shuffle.slice(26, 52);
 console.log("Hand one: ", handOne);
 console.log("Hand two: ", handTwo);
 
-console.log(cards)
-let warGame = (handOne, handTwo) => {
+// console.log(cards)
+// let warGame = (handOne, handTwo) => {
   
-  console.log(handOne.pop(), handTwo.pop());
-};
-warGame(handOne, handTwo);
-warGame(handOne, handTwo);
-warGame(handOne, handTwo);
-warGame(handOne, handTwo);
-warGame(handOne, handTwo);
-warGame(handOne, handTwo);
+//   console.log(handOne.pop(), handTwo.pop());
+// };
+// warGame(handOne, handTwo);
+// warGame(handOne, handTwo);
+// warGame(handOne, handTwo);
+// warGame(handOne, handTwo);
+// warGame(handOne, handTwo);
+// warGame(handOne, handTwo);
